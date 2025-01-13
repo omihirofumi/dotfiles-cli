11db16fbaa87658add0b27e89be42c5c17935f09
Hirofumi Omi <hirofumi.omi@3-shake.com>
2025-01-13 17:18:44 +0900

    remove unuseds

--- src/main.rs
+++ src/main.rs
@@ -9,11 +9,6 @@ use std::{fs, path::Path};
 fn main() {
     let config = Config::load(String::from("./dotfiles.toml"));
     config.init();
-    // config.dotfiles().iter().for_each(|dotfile| {
-    //     let dest_dir = Path::new(&config.dir()).join(dotfile.name());
-    //     fs::create_dir_all(&dest_dir).unwrap();
-    //     copy_path(&dest_dir.to_string_lossy(), dotfile.src(), dotfile.dest());
-    // });
     for (key, value) in config.dotfiles() {
         let dest_dir = Path::new(&config.dir()).join(key);
         fs::create_dir_all(&dest_dir).unwrap();
